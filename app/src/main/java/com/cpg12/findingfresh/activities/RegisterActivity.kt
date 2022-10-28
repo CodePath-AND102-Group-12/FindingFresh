a90b11f8136b4aac6c303e6aa7629252591f41eb
Kenneth Ng <kng11@wgu.edu>
2022-10-28 12:20:12 -0400

    Added a register page

---
+++ app/src/main/java/com/cpg12/findingfresh/activities/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.cpg12.findingfresh.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cpg12.findingfresh.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
