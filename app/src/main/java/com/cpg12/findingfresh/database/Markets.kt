1e01d9ef60f05297adabd68be387d28cff7cc9a1
Nic Ortiz <ortiz.nagare@gmail.com>
2022-10-31 20:44:08 -0400

    added vars that match what is in Firestore vs Realtime Database

--- app/src/main/java/com/cpg12/findingfresh/database/Markets.kt
+++ app/src/main/java/com/cpg12/findingfresh/database/Markets.kt
@@ -1,6 +1,10 @@
 package com.cpg12.findingfresh.database
 
+import com.google.firebase.firestore.GeoPoint
+import java.lang.ref.Reference
+
 data class Markets(
+    // these are the current ones used to move to Realtime database
     var marketName : String ? = null,
     var marketLocation : String ? = null,
     var marketEmail : String ? = null,
@@ -12,4 +16,12 @@ data class Markets(
     var marketOpenTime: String ? = null,
     var marketCloseDate: String ? = null,
     var marketCloseTime: String ? = null
+
+    // these are all that is currently kept in Firestore
+//    var address: String ? = null,
+//    var category: String ? = null,
+//    var image: Reference<String> ? = null,
+//    var location: GeoPoint ? = null,
+//    var name: String ? = null,
+//    var phone: String ? = null
 )
