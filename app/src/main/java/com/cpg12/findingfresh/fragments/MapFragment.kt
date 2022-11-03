092017c07861c3ed5f41952cbc0feb4f07b2f7e7
Nic Ortiz <ortiz.nagare@gmail.com>
2022-11-03 01:17:23 -0400

    added list of markets to Map fragment

--- app/src/main/java/com/cpg12/findingfresh/fragments/MapFragment.kt
+++ app/src/main/java/com/cpg12/findingfresh/fragments/MapFragment.kt
@@ -1,17 +1,20 @@
 package com.cpg12.findingfresh.fragments
 
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.cpg12.findingfresh.R
+import com.cpg12.findingfresh.database.Markets
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class MapFragment : Fragment(), OnMapReadyCallback {
@@ -25,15 +28,45 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         val supportMapFragment =
             childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         supportMapFragment?.getMapAsync(this)
+
+
+
         return view
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val geocoder = Geocoder(view?.context)
+
+        val firestore = FirebaseFirestore.getInstance()
+        val docRef = firestore.collection("farms")
+        val farmsData = ArrayList<Markets>()
+
+        docRef.get().addOnSuccessListener { documents ->
+            // add each of the markets to the list and create map markers for them
+            for (document in documents) {
+                val farm = document.toObject(Markets::class.java)
+                farmsData.add(farm)
+                println("Added ${farm.marketName} to farmsdata list")
+                val geoCoderResults = geocoder.getFromLocationName(farm?.marketLocation, 1)
+                val marketDetailLatitude = geoCoderResults.get(0).latitude
+                val marketDetailLongitude = geoCoderResults.get(0).longitude
+
+                farm?.marketLocation?.let {
+                    val loc = LatLng (marketDetailLatitude, marketDetailLongitude)
+                    mMap.addMarker(MarkerOptions().position(loc).title(farm.marketName))
+                    println("Market ${farm.marketName} added to map")
+                }
+            }
+
+            // use the last market added to determine the focus for the map
+            val lastMarket = farmsData[farmsData.size - 1]
+            val lastMarketGCR = geocoder.getFromLocationName(lastMarket?.marketLocation, 1)
+            val lastMarketLatLng = LatLng(lastMarketGCR.get(0).latitude, lastMarketGCR.get(0).longitude)
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastMarketLatLng,10f))
+        }
+
+        mMap.uiSettings.isZoomControlsEnabled = true
     }
 }
