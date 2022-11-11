a71b1fe988f7c154d8c953f9631519b716007154
Nic Ortiz <ortiz.nagare@gmail.com>
2022-11-10 20:46:52 -0500

    showing current user location (blue dot) on map listing

--- app/src/main/java/com/cpg12/findingfresh/fragments/MapFragment.kt
+++ app/src/main/java/com/cpg12/findingfresh/fragments/MapFragment.kt
@@ -1,12 +1,18 @@
 package com.cpg12.findingfresh.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.location.Geocoder
+import android.location.Location
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.cpg12.findingfresh.R
 import com.cpg12.findingfresh.activities.MainActivity
@@ -20,8 +26,11 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
 
-
-class MapFragment : Fragment(), OnMapReadyCallback {
+// requests for permissions for fine and coarse location to display the blue dot for the current location on the map fragment so that user can see where they are in relation to the markets
+class MapFragment : Fragment(), GoogleMap.OnMyLocationButtonClickListener,
+    GoogleMap.OnMyLocationClickListener, OnMapReadyCallback,
+    ActivityCompat.OnRequestPermissionsResultCallback {
+    private var permissionDenied = false
     private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
@@ -41,6 +50,9 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        googleMap.setOnMyLocationButtonClickListener(this)
+        googleMap.setOnMyLocationClickListener(this)
+        enableMyLocation()
 
         // cheating to move camera away from Africa right away
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(26.1187555,-80.176346),11f))
@@ -86,6 +98,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             val lastMarketLatLng = LatLng(lastMarketGCR[0].latitude, lastMarketGCR[0].longitude)
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastMarketLatLng,11f))
 
+
             // Notification varies based on whether one or multiple markets are open for today
             if (marketToday) {
                 if (marketsForToday > 1) {
@@ -98,6 +111,122 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
 
         mMap.uiSettings.isZoomControlsEnabled = true
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+
+        // 1. Check if permissions are granted, if so, enable the my location layer
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+            return
+        }
+
+        // 2. If if a permission rationale dialog should be shown
+//        if (ActivityCompat.shouldShowRequestPermissionRationale(
+//                requireActivity(),
+//                Manifest.permission.ACCESS_FINE_LOCATION
+//            ) || ActivityCompat.shouldShowRequestPermissionRationale(
+//                requireActivity(),
+//                Manifest.permission.ACCESS_COARSE_LOCATION
+//            )
+//        ) {
+//            PermissionUtils.RationaleDialog.newInstance(
+//                LOCATION_PERMISSION_REQUEST_CODE, true
+//            ).show(supportFragmentManager, "dialog")
+//            return
+//        }
+
+        // 3. Otherwise, request permission
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false
+    }
+
+    override fun onMyLocationClick(location: Location) {
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            super.onRequestPermissionsResult(
+                requestCode,
+                permissions,
+                grantResults
+            )
+            return
+        }
+
+        if (isPermissionGranted(
+                permissions,
+                grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) || isPermissionGranted(
+                permissions,
+                grantResults,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation()
+        } else {
+            // Permission was denied. Display an error message
+            // Display the missing permission error dialog when the fragments resume.
+            permissionDenied = true
+        }
+    }
+
+
+    companion object {
+        /**
+         * Request code for location permission request.
+         *
+         * @see .onRequestPermissionsResult
+         */
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+        /**
+         * Checks if the result contains a [PackageManager.PERMISSION_GRANTED] result for a
+         * permission from a runtime permissions request.
+         *
+         * @see androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
+         */
+        @JvmStatic
+        fun isPermissionGranted(
+            grantPermissions: Array<String>, grantResults: IntArray,
+            permission: String
+        ): Boolean {
+            for (i in grantPermissions.indices) {
+                if (permission == grantPermissions[i]) {
+                    return grantResults[i] == PackageManager.PERMISSION_GRANTED
+                }
+            }
+            return false
+        }
     }
 
     private fun checkDate(today : String?, market: Markets) : Boolean {
